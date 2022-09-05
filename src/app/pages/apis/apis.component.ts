fabefeb319c5ef0771a5980eada2f72304b0677c
Niall Thurrat <nt222fc@student.lnu.se>
2022-09-05 23:43:53 +0200

    refactor

--- src/app/pages/apis/apis.component.ts
+++ src/app/pages/apis/apis.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApisService } from '../../services/apis.service';
 import { Api } from '../../models/api';
-import { ApisDto } from '../../models/apis-dto';
 
 @Component({
   selector: 'app-apis',
@@ -14,7 +13,7 @@ export class ApisComponent implements OnInit {
   constructor(private apisService: ApisService) {}
 
   ngOnInit(): void {
-    this.apisService.getApis().subscribe((response: ApisDto) => {
+    this.apisService.getApis().subscribe((response) => {
       this.apis = response.entries;
     });
   }
