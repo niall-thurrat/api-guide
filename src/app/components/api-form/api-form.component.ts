d6fdb6e1ecc38fa22aa0365a45d2aab709079b32
Niall Thurrat <nt222fc@student.lnu.se>
2022-10-06 10:37:35 +0200

    demo alerts added

--- src/app/components/api-form/api-form.component.ts
+++ src/app/components/api-form/api-form.component.ts
@@ -15,15 +15,18 @@ export class ApiFormComponent {
   constructor(private apisService: ApisService, private router: Router) { }
 
   addApi(): void {
+    alert('Great to see you\'re having a look at my demo app! This add function is just for show. It is not supported by the (backend) API.');
     this.apisService.add(this.api);
     this.router.navigate(['/apis']);
   }
 
   updateApi(): void {
+    alert('Great to see you\'re having a look at my demo app! This update function is just for show. It is not supported by the (backend) API.');
     this.apisService.update(this.api);
   }
 
   deleteApi(): void {
+    alert('Great to see you\'re having a look at my demo app! This delete function is just for show. It is not supported by the (backend) API.');
     this.apisService.delete(this.api.API);
     this.router.navigate(['/apis']);
   }
